c151dabcaf9eb531b346009fc2a235a4b295adc8
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-02-20 19:05:37 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import './App.css';
+import { useState } from 'react';
+import Card from './components/card';
+
+function App() {
+
+
+  const [people, updatePeople] = useState([
+    { id: 1, name: "Kasie Ugwu", role: "Frontend Engineer", description: ["Kind, ", "Smart, ", "Funny"], profilePic: "https://t4.ftcdn.net/jpg/02/79/66/93/360_F_279669366_Lk12QalYQKMczLEa4ySjhaLtx1M2u7e6.jpg" },
+    { id: 2, name: "Glory Okafor", role: "Backend Engineer", description: ["Humble, ", "Sincere, ", "Likeable"], profilePic: "https://t3.ftcdn.net/jpg/04/67/95/40/360_F_467954064_qFJxeuLVPcXon6X478v9hdgHrRlNgzDB.jpg" },
+    { id: 3, name: "Oyeyemi Bunmi", role: "Product Designer", description: ["Resourceful, ", "Resilient, ", "Determined"],profilePic: "https://us.123rf.com/450wm/moremar/moremar1904/moremar190400023/moremar190400023.jpg?ver=6" },
+    { id: 4, name: "Valentina Onwuli", role: "DevOps Engineer", description: ["Thoughtful, ", "Friendly, ", "Outgoing"], profilePic: "https://thumbs.dreamstime.com/b/indian-woman-face-avatar-cartoon-indian-woman-face-makeup-earrings-profile-picture-avatar-cartoon-character-portrait-150832491.jpg" },
+    { id: 5, name: "Ibraheem Ejibode", role: "Product Manager", description: ["Persuasive, ", "Adventurous, ", "Audacious"], profilePic: "https://static.vecteezy.com/system/resources/thumbnails/002/275/847/small/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg" }
+  ])
+
+
+  const handleDelete = (id) =>{
+    let temp = people.filter(each => each.id !== id)
+    updatePeople(temp)
+  }
+
+  // const openModal = (id) =>{
+  //   let temp = people.filter(each => each.id !== id)
+  //   updatePeople(temp)
+  // } 
+
+  return (
+    <div style={{ color: 'blue' }} className="App">
+      <div className="container mt-5">
+        <div className="row">
+          {people.map(item =>{
+            return(<Card key={item.id} name={item.name} role={item.role} description={item.description} profilePic={item.profilePic} id={item.id} handleDelete={handleDelete} />)
+          })}
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,52 @@
+import './card.css'
+
+let modal = document.getElementById("myModal");
+
+const openModal = (id) =>{
+    modal.style.display = "block";
+  } 
+
+const closeModal = (id) =>{
+    modal.style.display = "none";
+  }
+
+const Card = ({name, role, description, profilePic, handleDelete, id}) => {
+    return (
+        <div className="profile">
+            <div className="col-lg-3 mb-2 profile-card">
+                <div className="card">
+                    <div className="close" onClick={()=> handleDelete(id)}>
+                        X
+                    </div>
+                    <div>
+                        <img className="img-responsive profilePic" 
+                        src={profilePic}
+                        alt="profile pic" />
+                    </div>
+                    <div className="descr">
+                        <p className="name"> {name}</p>
+                        <button className="profileBtn" onClick={()=> openModal(id)}> Click to view profile</button>
+                    </div>
+                </div>
+            </div>
+
+            <div id="myModal" className="modal">
+                <div className="modal-content">
+                    <span className="closebtn" onClick={()=> closeModal(id)}>&times;</span>
+                    <div className="img">
+                        <img className="modal-img" src={profilePic} alt="profile pic" />
+                    </div>
+                    <div className="modal-body">
+                        <ul>
+                            <ol>Name: {name}</ol>
+                            <ol>Role: {role}</ol>
+                            <ol>Description: {description}</ol>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+       </div> 
+    )
+}
+
+export default Card;
